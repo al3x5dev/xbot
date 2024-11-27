01fa0113d284103c86ca78af2dd1b46c8e4d24b0
alexsandrov16 <al3x5.dev@gmail.com>
2024-11-27 01:08:51 -0500

    Added message

--- src/Cli/Commands/Help.php
+++ src/Cli/Commands/Help.php
@@ -12,34 +12,56 @@ use Al3x5\xBot\Cli\Style;
  */
 final class Help extends Cmd
 {
+    private static array $commands = [
+        'help    ' => "Show this screen.\n",
+        'install ' => "Create bot configuration files\n",
+        'hook    ' => "Manages the webhook settings of your Telegram bot (see options)",
+    ];
+
+    private static array $options = [
+        'set     ' => "Set the webhook\n",
+        'get     ' => "Get the webhook information\n",
+        'delete  ' => "Delete the webhook\n",
+        'about   ' => "Get the bot's information\n",
+    ];
+
     public static function execute(array $argv = []): string
     {
+        //chequea si el usuario ha proporcionado algún argumento
+        $ck = self::checkArguments($argv);
+        if (!is_null($ck)) return $ck;
+
         $app = self::NAME;
         $version = self::VERSION;
 
+        $color = function ($value, $colors): string {
+            return Style::color($value, $colors);
+        };
 
         $banner = <<<CMD
-        Welcome to $app v$version
+        $app v$version
         
-        Usage:
-        php xbot [options] [arguments]
+        {$color('Usage:', 'yellow')}
+           php xbot [commands] [options]
         
-        Options:
+        {$color('Commands:', 'yellow')}
         CMD;
         self::println($banner);
 
-        $commands = [
-            'help   ' => "sd\n",
-            'install' => "dfff\n",
-            'hook   ' => "sdff\n",
-        ];
-
-        foreach ($commands as $key => $value) {
-            self::printl("$key    ".Style::color($value,'green'));
+        //Comandos
+        foreach (static::$commands as $key => $value) {
+            if (is_string($value)) {
+                print($color("   $key      ", 'green') . $value);
+            }
         }
 
+        // Opciones
+        self::println(PHP_EOL.PHP_EOL.$color('Options:', 'yellow'));
+        //self::println($color('   hook', 'green'));
 
-
+        foreach (static::$options as $k => $v) {
+            print($color("     $k    ", 'green') . $v);
+        }
         return '';
     }
 }
--- src/Cli/Commands/Hook.php
+++ src/Cli/Commands/Hook.php
@@ -10,19 +10,28 @@ use Al3x5\xBot\Cli\Style;
  */
 final class Hook extends Cmd
 {
-    private array $options=[
-        'help',                        // help message
-        'set',                         // setWebhook
-        'get',                         // getWebhookInfo
-        'delete',                      // deleteWebhook
-        'about',                       // getMe
-
-
-
+    private static array $options = [
+        'set    ' => "Set the webhook\n",                 // setWebhook
+        'get    ' => "Get the webhook information\n",     // getWebhookInfo
+        'delete ' => "Delete the webhook\n",           // deleteWebhook
+        'about  ' => "Get the bot's information\n",     // getMe
     ];
 
     public static function execute(array $argv = []): string
     {
-        return self::println(Style::color('Hook Command','green'));
+        self::println(
+            Style::bgColor("Manages the webhook settings of your Telegram bot", "cyan",false) .PHP_EOL .PHP_EOL .
+                Style::color('Usage:', 'yellow') . PHP_EOL .
+                "php xbot hook [options]"
+        );
+
+        print(PHP_EOL . Style::color('Options:', 'yellow') . "\n");
+
+        foreach (static::$options as $key => $value) {
+            if (is_string($value)) {
+                print(Style::color("   $key    ", 'green') . $value);
+            }
+        }
+        return '';
     }
 }
