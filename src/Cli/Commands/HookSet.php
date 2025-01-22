b2491a0b853f1aba34475db7675ea1ab20b235d7
alexsandrov16 <al3x5.dev@gmail.com>
2025-01-22 11:00:16 -0500

    Added URL validation functionality

--- src/Cli/Commands/HookSet.php
+++ src/Cli/Commands/HookSet.php
@@ -6,6 +6,7 @@ use Al3x5\xBot\Cli\Cmd;
 use Al3x5\xBot\Cli\Commands\Traits\HookTrait;
 use Al3x5\xBot\Cli\Style;
 use Al3x5\xBot\xBot;
+use Mk4U\Http\Uri;
 
 /**
  * Set Bot class
@@ -24,22 +25,32 @@ final class HookSet extends Cmd
 
         self::isConfig($config);
 
-        self::println("Use an empty string to remove the webhook integration\n");
-
-        $xbot = new xBot(require $config);
-
+        //Mensaje a mostrar en caso de que no se pase url como argumento
         $arg = '';
         if (empty($argv[2])) {
             $arg = self::input('HTTPS URL to send updates:');
         } else {
             $arg = $argv[2];
         }
-
-
-
-        $data = $xbot->setWebhook([
+        
+        //verifica la url pasada por el usuario
+        if (!self::isURL($arg)) {
+            return self::println(Style::bgColor('error','red').' invalid URL');
+        }
+        
+        //Enviar peticion a telegram
+        $data = (new xBot(require $config))->setWebhook([
             'url' => $arg,
         ]);
         return self::println(Style::bgColor($data, 'green'));
+    }
+
+    /**
+     * Verifica si la url pasada es valida
+     */
+    protected static function isURL(string $url): bool
+    {
+        $uri = new Uri($url);
+        return !($url === '' || $uri->getScheme() != 'https');
     }
 }
