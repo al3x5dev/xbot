3540b64ff62def84c1fa6366a3825168fe08239c
alexsandrov16 <al3x5.dev@gmail.com>
2025-01-29 16:18:11 -0500

    Refactor: Add property definitions in Callback, InlineQuery, Chat and InaccessibleMessage entities

--- src/Entities/Chat.php
+++ src/Entities/Chat.php
@@ -7,7 +7,46 @@ namespace Al3x5\xBot\Entities;
  */
 class Chat extends Base
 {
-    public function getEntities() : array
+    /**
+     * Unique identifier for this chat.
+     * 
+     * This number may have more than 32 significant bits and some programming languages 
+     * may have difficulty/silent defects in interpreting it. But it has at most 52 significant bits, 
+     * so a signed 64-bit integer or double-precision float type are safe for storing this identifier.
+     */
+    public int $id;
+
+    /**
+     * Type of the chat, can be either “private”, “group”, “supergroup” or “channel”
+     */
+    public string $type;
+
+    /**
+     * Optional. Title, for supergroups, channels and group chats
+     */
+    public string $title;
+
+    /**
+     * Optional. Username, for private chats, supergroups and channels if available
+     */
+    public string $username;
+
+    /**
+     * Opcional . Nombre de la otra fiesta en una charla privada
+     */
+    public string $first_name;
+
+    /**
+     * Opcional . Apellido de la otra fiesta en una charla privada
+     */
+    public string $last_name;
+
+    /**
+     * Opcional . Es cierto , si el chat de supergrupo es un foro (tiene temas habilitados) 
+     */
+    public bool $is_forum;
+
+    public function getEntities(): array
     {
         return [];
     }
