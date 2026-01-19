57f169a252fb3f4ccda11a82162fdfcdf72bc403
al3x5dev <al3x5.dev@gmail.com>
2026-01-19 01:07:26 -0500

    refactor(commands): replace getDescription() with static description() and improve args API

--- src/Commands.php
+++ src/Commands.php
@@ -12,35 +12,68 @@ use Al3x5\xBot\Traits\Responder;
 abstract class Commands
 {
     public ?Message $message;
+    private array $args = [];
     use Responder;
 
     public function __construct(protected Update $update)
     {
-        $this->update = $update;
         $this->message = $update->getMessage();
     }
 
     /**
      * Devuelve lista de comandos
      */
-    public function getCommandsList(): array
+    protected function getCommandsList(): array
     {
         $commands = [];
 
         foreach ($this->getAllCommands() as $name => $className) {
-            $commands[$name] = (new $className($this->update))->getDescription();
+            $commands[$name] = $className::description();
         }
 
         return $commands;
     }
 
     /**
+     * Establece argumentos
+     */
+    final public function setArgs(array $args): void
+    {
+        $this->args = $args;
+    }
+
+    /**
+     * Devuelve un argumento especificado
+     */
+    protected function arg(int $index, mixed $default = null): mixed
+    {
+        return $this->args[$index] ?? $default;
+    }
+
+    /**
+     * Devuelve argumentos
+     */
+    protected function args(?int $count = null): array
+    {
+        if ($count === null) {
+            return $this->args;
+        }
+
+        return array_slice(
+            $this->args + array_fill(0, $count, null),
+            0,
+            $count
+        );
+    }
+
+
+    /**
      * Ejecuta el comando
      */
-    abstract public function execute(...$params): void;
+    abstract public function execute(): void;
 
     /**
      * Help command
      */
-    abstract public function getDescription(): string;
+    abstract public static function description(): string;
 }
